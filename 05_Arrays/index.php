bef66155dd324685c35491a9c61e0c3fcdaafe4d
Elnur <entreur@gmail.com>
2023-08-06 13:46:36 +0400

    Arrays of Php

---
+++ 05_Arrays/index.php
@@ -0,0 +1,15 @@
+<?php 
+    define("Title","Lesson 05 - Arrays");
+    $students = array("James","Walter","Saul");
+?>
+
+<html>
+<head>
+    <title><?=Title?></title>
+<body>
+    <br> <?php echo($students[0]) ?> 
+    <br> <?php echo($students[1]) ?> </br>
+    <?php echo($students[2]) ?> 
+</body>
+</head>
+</html>
