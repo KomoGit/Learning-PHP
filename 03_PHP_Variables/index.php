d8a941bc524891776b0dd8d809dca395d03d753c
Elnur <entreur@gmail.com>
2023-08-06 13:10:57 +0400

    Variables.

---
+++ 03_PHP_Variables/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Lesson 03 - Variables</title>
+<body>
+    <?php
+    $HL = "Hello World";
+    $int = 23;
+    $float = 1200.10;
+    $str = "James";
+
+        echo("$HL my name is $str, I am $int years old and my salary is $float") 
+    ?>
+</body>
+</head>
+</html>
